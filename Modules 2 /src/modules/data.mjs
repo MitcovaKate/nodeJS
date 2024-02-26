f58ce2855e4d1ca4832895a3e40b582559232339
Mitcova Kate <katya.mitkova@bk.ru>
2024-02-27 00:42:35 +0200

    Create data.mjs

---
+++ Modules 2 /src/modules/data.mjs
@@ -0,0 +1,6 @@
+const products = [
+  { name: "Product 1", price: 200 },
+  { name: "Another Product 2", price: 2100 },
+  { name: "Yep ,Product 3", price: 455600 },
+];
+export default { products };
