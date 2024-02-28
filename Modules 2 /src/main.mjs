b7c991ff72853c6a56e695e3dcde9328de4f6ef9
Mitcova Kate <katya.mitkova@bk.ru>
2024-02-28 18:16:57 +0200

    Update main.mjs

--- Modules 2 /src/main.mjs
+++ Modules 2 /src/main.mjs
@@ -1,3 +1,8 @@
 import ui from './modules/ui.mjs'
 import data from "./modules/data.mjs"
-ui.renderCatatlog(data.products)
+ui.renderCatatlog(data.products,(n,product,q)=>{
+    cart.items.push({ n, product, q });
+    // console.log(n, product, q);
+console.log(cart)
+    
+})
