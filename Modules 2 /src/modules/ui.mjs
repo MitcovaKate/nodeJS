d4e6a9deecead959cf05057091833a06482433f2
Mitcova Kate <katya.mitkova@bk.ru>
2024-02-28 18:18:25 +0200

    Update ui.mjs

--- Modules 2 /src/modules/ui.mjs
+++ Modules 2 /src/modules/ui.mjs
@@ -1,6 +1,14 @@
 import {helper} from "./helper.mjs";
+import readline from 'readline'
 
-const renderCatatlog = (products) => {
+const io=readline.createInterface(
+  {
+    input:process.stdin,
+    output:process.stdout
+  }
+)
+
+const renderCatatlog = (products,cart,confirmCB) => {
   console.clear();
 helper("Catalog:")
   // products.forEach((product,idx)=>{
@@ -14,7 +22,44 @@ helper("Catalog:")
 
     console.log(`${idxStr} ${nameStr} ${priceStr}`);
   });
-  console.log("choose >");
+  io.question("choose > ",answer=>{
+    let n =parseInt(answer)
+     if (isNaN(n)) {
+       console.error("It is not a number!");
+       return;
+     }
+    if(1 <= n && n <= products.length){
+      let product =products[n-1]
+      io.question(`How many "${product.name}":? `,answer=>{
+        let q=parseInt(answer)
+        if (isNaN(q)) {
+         console.error("It is not a number!");
+         return;
+          }
+
+         let cost =q * product.price
+         io.question(`Product cost = "${cost}": confirm (y/n) `,answer=>{
+          switch (answer) {
+            case "y":
+              // Add product to cart
+              cart.items.push({ n, product, q });
+              console.log("Product added to cart!");
+              confirmCB(cart); //  updated cart to callback
+              
+              break;
+            case "n":
+              console.log(`product "${product.name}" was not confirmes!`);
+              break;
+            default:
+              console.log("Invalid option");
+              break;
+          }
+          io.close();
+         }) 
+      
+      })
+    }
+  });
 };
 
 export default { renderCatatlog };
