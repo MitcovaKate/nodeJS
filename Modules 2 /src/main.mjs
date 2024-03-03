a039c619ae1c9e6d31ca6ecb7815d40a8e4f3ead
Mitcova Kate <katya.mitkova@bk.ru>
2024-03-03 23:43:57 +0200

    Update main.mjs

--- Modules 2 /src/main.mjs
+++ Modules 2 /src/main.mjs
@@ -1,8 +1,8 @@
-import ui from './modules/ui.mjs'
-import data from "./modules/data.mjs"
-ui.renderCatatlog(data.products,(n,product,q)=>{
-    cart.items.push({ n, product, q });
-    // console.log(n, product, q);
-console.log(cart)
+import {renderMainMenu} from './modules/ui.mjs'
+import { products, cart} from "./modules/data.mjs"
+// renderCatatlog(products,(n,product,q)=>{
+//     cart.items.push({ n, product, q });
+// console.log(cart)
     
-})
+// })
+renderMainMenu()
